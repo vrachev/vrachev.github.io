fe181e2750e87c3da0b0948b530b41d0c826603b
vrachev <vrachev96@gmail.com>
2024-03-25 13:19:57 -0400

    add github actions deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vrachev.github.io',
   integrations: [tailwind()],
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
